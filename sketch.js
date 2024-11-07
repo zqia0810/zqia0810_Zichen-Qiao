4fb59b32dae5ddf559f45f8e7417104bf3e7873d
zqia0810 <zqia0810@uni.sydney.edu.au>
2024-11-07 23:39:37 +1100

    Update sketch.js
    Add waves, water lines, clouds, buildings and reflections

--- sketch.js
+++ sketch.js
@@ -31,4 +31,36 @@ function setup() {
     let waterColor = lerpColor(color(255, 150, 100), color(0, 100, 150), (y - height / 2) / (height / 2));
     shapes.push(new BauhausRect(0, y, width, 10, waterColor));
   }
+
+  // Adding water lines with ripple effect
+  for (let i = 0; i < width; i += 15) {
+    let y = height / 2 + 50; // Water surface position
+    shapes.push(new BauhausLine(i, y, 100, 0, color(252, 147, 55, 80), i));
+  }
+
+ 
+  // Add cloud
+  shapes.push(new BauhausCloud(100, 70, 90, 200)); // The first cloud
+  shapes.push(new BauhausCloud(300, 50, 120, 230)); // The second cloud
+  shapes.push(new BauhausCloud(500, 100, 100, 180)); // The third cloud
+  shapes.push(new BauhausCloud(650, 80, 130, 200)); // The fourth cloud
+  shapes.push(new BauhausCloud(750, 120, 85, 220)); // The fifth cloud
+  
+
+  // Add building
+  shapes.push(new BauhausRect(220, 80, 30, 100, color(0, 0, 0))); //left part
+  shapes.push(new BauhausRect(210, 120, 50, 80, color(20, 10, 60))); // middle part
+  shapes.push(new BauhausRect(190, 180, 300, 120, color(50, 30, 80))); // right part
+
+  //  Add the reflection of the building in the water
+  shapes.push(new BauhausRect(220, 300, 80, 200, color(0, 0, 0, 80))); // top reflection, transparent black
+  shapes.push(new BauhausRect(230, 450, 50, 80, color(20, 10, 60, 80))); // middle reflection, transparent purple
+  shapes.push(new BauhausRect(250, 500, 30, 60, color(150, 30, 80, 80))); // bottom reflection, transparent red
+
+
+  // Control the update of the sun's position every 50 milliseconds.
+  setInterval(updateSunPosition, 50);
+
+  // Switching to the next window every 500 milliseconds.
+  setInterval(updateWindowLights, 500);
 }
